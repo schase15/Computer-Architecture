288f2d523a462387ec3dc8293936c764723a396a
Steven Chase <stevenchase@Stevens-MacBook-Pro.local>
2020-08-17 19:26:50 -0400

    Day 1 MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,14 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        # 8 registers
+        self.reg = [0] * 8
+        # Point counter
+        self.pc = 0
+        # Memory with 256 bits
+        self.ram = [0b0] * 256
+
+
 
     def load(self):
         """Load a program into memory."""
@@ -62,4 +69,42 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+        
+        while running:
+            # Set the instruction register using the point counter
+            ir = self.ram[self.pc]
+            # save the next two operands; some programs will use them
+            operand_a = self.ram[self.pc +1]
+            operand_b = self.ram[self.pc +2]
+            
+            # Tell the CPU what to do when given a specific instruction
+            # HLT
+            if ir == 0b00000001:
+                # Stop the program
+                running = False
+                # Increment counter, even though the program is stopped
+                self.pc += 1
+
+            # LDI - set the value of a register to an integer
+            # Uses pc and pc+1, pc+2
+            elif ir == 0b10000010:
+                # Use operand_a and operand_b
+                self.reg[operand_a] = operand_b
+                # Increase pc by 3
+                self.pc += 3
+            # PRN - Print the value stored in the given register
+            # Use operand_a
+            elif ir == 0b01000111:
+                # print value in register
+                print(self.reg[operand_a])
+                self.pc += 2
+
+    def ram_read(self, address):
+        """Accept the address to read and return the value stored there"""
+        return self.ram[address]
+
+    def ram_write(self, address, value):
+        """Accept a value to write and the address to write it to"""
+        self.ram[address] = value
+    
